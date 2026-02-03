86fd8edb8f97be4051564779ec0b1910b78abed4
alv-around <alfmail@pm.me>
2026-02-03 11:16:08 +0100

    field addition & multiplication

--- src/ff.rs
+++ src/ff.rs
@@ -1,3 +1,5 @@
+use core::ops::{Add, Mul};
+
 // INFO: source: https://eprint.iacr.org/2017/634.pdf
 const KYBER_Q: u32 = 7681; // TODO: support u16
 // TODO: find source
@@ -22,6 +24,28 @@ impl<const Q: u32> PartialEq for FiniteField<Q> {
     }
 }
 
+#[allow(clippy::suspicious_arithmetic_impl)]
+impl<const Q: u32> Add for FiniteField<Q> {
+    type Output = FiniteField<Q>;
+
+    // strict because we know sofar that no sum of 2 Fields would overflow.
+    // Kyber & Dlitiums qs are far from 2^32
+    fn add(self, rhs: Self) -> Self::Output {
+        Self(self.0.strict_add(rhs.0) % Q)
+    }
+}
+
+#[allow(clippy::suspicious_arithmetic_impl)]
+impl<const Q: u32> Mul for FiniteField<Q> {
+    type Output = FiniteField<Q>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        let mult_expanded = (self.0 as u64).strict_mul(rhs.0 as u64);
+        let mod_mult = mult_expanded % (Q as u64);
+        Self(mod_mult as u32)
+    }
+}
+
 pub type KyberFf = FiniteField<KYBER_Q>;
 pub type DlithiumFf = FiniteField<DLITHIUM_Q>;
 
@@ -30,7 +54,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_wrapping() {
+    fn test_field_wrapping() {
         assert!(DLITHIUM_Q < i32::MAX as u32);
 
         //zero
@@ -47,4 +71,15 @@ mod test {
         assert!(KyberFf::minus(KYBER_Q + 1) == KyberFf::minus(1));
         assert!(DlithiumFf::minus(DLITHIUM_Q + 1) == DlithiumFf::minus(1));
     }
+
+    #[test]
+    fn test_field_addition() {
+        assert!(KyberFf::new(1) + KyberFf::new(1) == KyberFf::new(2));
+        assert!(KyberFf::minus(1) + KyberFf::new(1) == KyberFf::new(0));
+        assert!(DlithiumFf::new(1) + DlithiumFf::new(1) == DlithiumFf::new(2));
+        assert!(DlithiumFf::minus(1) + DlithiumFf::new(1) == DlithiumFf::new(0));
+    }
+
+    #[test]
+    fn test_field_multiplication() {}
 }
