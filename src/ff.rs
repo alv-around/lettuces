c43ac4b58acf5f171544eaeb9b686ddbe0dc4a03
alv-around <alfmail@pm.me>
2026-02-03 09:49:17 +0100

    better negative fields

--- src/ff.rs
+++ src/ff.rs
@@ -6,14 +6,13 @@ const DLITHIUM_Q: u32 = 8380417;
 pub struct FiniteField<const Q: u32>(u32);
 
 impl<const Q: u32> FiniteField<Q> {
-    fn new(x: i32) -> Self {
-        let x = if x.is_positive() {
-            x as u32 % Q
-        } else {
-            let q = Q as i32;
-            (((x % q) + q) % q) as u32
-        };
-        Self(x)
+    pub fn new(x: u32) -> Self {
+        Self(x % Q)
+    }
+
+    pub fn minus(x: u32) -> Self {
+        let i = x % Q;
+        Self(Q - i)
     }
 }
 
@@ -35,15 +34,17 @@ mod test {
         assert!(DLITHIUM_Q < i32::MAX as u32);
 
         //zero
-        assert!(KyberFf::new(KYBER_Q as i32) == KyberFf::new(0));
-        assert!(DlithiumFf::new(DLITHIUM_Q as i32) == DlithiumFf::new(0));
+        assert!(KyberFf::new(KYBER_Q) == KyberFf::new(0));
+        assert!(DlithiumFf::new(DLITHIUM_Q) == DlithiumFf::new(0));
 
         // one
-        assert!(KyberFf::new(KYBER_Q as i32 + 1) == KyberFf::new(1));
-        assert!(DlithiumFf::new(DLITHIUM_Q as i32 + 1) == DlithiumFf::new(1));
+        assert!(KyberFf::new(KYBER_Q + 1) == KyberFf::new(1));
+        assert!(DlithiumFf::new(DLITHIUM_Q + 1) == DlithiumFf::new(1));
 
-        // minus one
-        assert!(KyberFf::new(KYBER_Q as i32 - 1) == KyberFf::new(-1));
-        assert!(DlithiumFf::new(DLITHIUM_Q as i32 - 1) == DlithiumFf::new(-1));
+        // minus
+        assert!(KyberFf::new(KYBER_Q - 1) == KyberFf::minus(1));
+        assert!(DlithiumFf::new(DLITHIUM_Q - 1) == DlithiumFf::minus(1));
+        assert!(KyberFf::minus(KYBER_Q + 1) == KyberFf::minus(1));
+        assert!(DlithiumFf::minus(DLITHIUM_Q + 1) == DlithiumFf::minus(1));
     }
 }
