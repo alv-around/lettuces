de9650b3caee24bfb717602248a8becef0c8c945
alv-around <alfmail@pm.me>
2026-02-04 16:47:34 +0100

    tests and sources

--- src/ff.rs
+++ src/ff.rs
@@ -2,7 +2,7 @@ use core::ops::{Add, Mul};
 
 // INFO: source: https://eprint.iacr.org/2017/634.pdf
 const KYBER_Q: u32 = 7681; // TODO: support u16
-// TODO: find source
+// INFO: source: https://eprint.iacr.org/2017/633.pdf
 const DLITHIUM_Q: u32 = 8380417;
 
 pub struct FiniteField<const Q: u32>(u32);
@@ -15,6 +15,14 @@ impl<const Q: u32> FiniteField<Q> {
     pub fn minus(x: u32) -> Self {
         let i = x % Q;
         Self(Q - i)
+    }
+
+    pub fn zero() -> Self {
+        FiniteField::new(0)
+    }
+
+    pub fn one() -> Self {
+        FiniteField::new(1)
     }
 }
 
@@ -52,18 +60,19 @@ pub type DlithiumFf = FiniteField<DLITHIUM_Q>;
 #[cfg(test)]
 mod test {
     use super::*;
+    use rand::random_range;
 
     #[test]
     fn test_field_wrapping() {
         assert!(DLITHIUM_Q < i32::MAX as u32);
 
         //zero
-        assert!(KyberFf::new(KYBER_Q) == KyberFf::new(0));
-        assert!(DlithiumFf::new(DLITHIUM_Q) == DlithiumFf::new(0));
+        assert!(KyberFf::new(KYBER_Q) == KyberFf::zero());
+        assert!(DlithiumFf::new(DLITHIUM_Q) == DlithiumFf::zero());
 
         // one
-        assert!(KyberFf::new(KYBER_Q + 1) == KyberFf::new(1));
-        assert!(DlithiumFf::new(DLITHIUM_Q + 1) == DlithiumFf::new(1));
+        assert!(KyberFf::new(KYBER_Q + 1) == KyberFf::one());
+        assert!(DlithiumFf::new(DLITHIUM_Q + 1) == DlithiumFf::one());
 
         // minus
         assert!(KyberFf::new(KYBER_Q - 1) == KyberFf::minus(1));
@@ -74,12 +83,53 @@ mod test {
 
     #[test]
     fn test_field_addition() {
-        assert!(KyberFf::new(1) + KyberFf::new(1) == KyberFf::new(2));
-        assert!(KyberFf::minus(1) + KyberFf::new(1) == KyberFf::new(0));
-        assert!(DlithiumFf::new(1) + DlithiumFf::new(1) == DlithiumFf::new(2));
-        assert!(DlithiumFf::minus(1) + DlithiumFf::new(1) == DlithiumFf::new(0));
+        // addition
+        assert!(KyberFf::one() + KyberFf::new(1) == KyberFf::new(2));
+        assert!(KyberFf::minus(1) + KyberFf::one() == KyberFf::zero());
+        assert!(DlithiumFf::one() + DlithiumFf::new(1) == DlithiumFf::new(2));
+        assert!(DlithiumFf::minus(1) + DlithiumFf::one() == DlithiumFf::zero());
+        assert!(KyberFf::minus(1) + KyberFf::new(2) == KyberFf::one());
+        assert!(DlithiumFf::minus(1) + DlithiumFf::new(2) == DlithiumFf::one());
+        assert!(KyberFf::new(2) + KyberFf::new(7) == KyberFf::new(9));
+        assert!(DlithiumFf::new(2) + DlithiumFf::new(7) == DlithiumFf::new(9));
+
+        // Addition by neutral element
+        assert!(KyberFf::new(11) + KyberFf::zero() == KyberFf::new(11));
+        assert!(DlithiumFf::new(11) + DlithiumFf::zero() == DlithiumFf::new(11));
+
+        // use random numbers
+        let a = random_range(0..=KYBER_Q);
+        let b = random_range(0..=KYBER_Q);
+        let c = a + b;
+        assert!(KyberFf::new(a) + KyberFf::new(b) == KyberFf::new(c));
+        let a = random_range(0..=DLITHIUM_Q);
+        let b = random_range(0..=DLITHIUM_Q);
+        let c = a + b;
+        assert!(DlithiumFf::new(a) + DlithiumFf::new(b) == DlithiumFf::new(c));
+
+        // substraction
+        assert!(KyberFf::one() + KyberFf::minus(1) == KyberFf::zero());
+        assert!(DlithiumFf::one() + DlithiumFf::minus(1) == DlithiumFf::zero());
     }
 
     #[test]
-    fn test_field_multiplication() {}
+    fn test_field_multiplication() {
+        // multiply by 0
+        assert!(KyberFf::new(17) * KyberFf::zero() == KyberFf::zero());
+        assert!(DlithiumFf::new(17) * DlithiumFf::zero() == DlithiumFf::zero());
+
+        // multiply by 1
+        assert!(KyberFf::new(17) * KyberFf::one() == KyberFf::new(17));
+        assert!(DlithiumFf::new(17) * DlithiumFf::one() == DlithiumFf::new(17));
+
+        // use random numbers
+        let a = random_range(0..=KYBER_Q) as u64;
+        let b = random_range(0..=KYBER_Q) as u64;
+        let c = (a * b) % (KYBER_Q as u64);
+        assert!(KyberFf::new(a as u32) * KyberFf::new(b as u32) == KyberFf::new(c as u32));
+        let a = random_range(0..=DLITHIUM_Q) as u64;
+        let b = random_range(0..=DLITHIUM_Q) as u64;
+        let c = (a * b) % (DLITHIUM_Q as u64);
+        assert!(DlithiumFf::new(a as u32) * DlithiumFf::new(b as u32) == DlithiumFf::new(c as u32));
+    }
 }
